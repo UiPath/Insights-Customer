bb160f803b125eeceb0d5e59524ce4b0585891f3
GengyiQin <gengyi.qin@uipath.com>
2022-09-15 15:04:49 -0700

    add index script into existing script

--- Scripts/SQLServer/IndexScripts/CreateLRWIndexScript.sql
+++
@@ -1,6 +0,0 @@
-IF NOT EXISTS(SELECT * FROM sys.indexes WHERE Name = 'IX_JobEvents_JobId_TenantId' and OBJECT_NAME(object_id) = 'JobEvents')
-CREATE NONCLUSTERED INDEX [IX_JobEvents_JobId_TenantId] ON [dbo].[JobEvents]
-(
-	[JobId] ASC,
-	[TenantId] ASC
-)
