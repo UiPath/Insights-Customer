6a1af262e55e14de28ae2a229445b67bc1596ef3
Hongjian Niu <hongjian.niu@uipath.com>
2022-09-28 15:44:43 -0700

    Remove

--- Scripts/SQLServer/DeleteByTenantKey/DeleteByTenantKey.sql
+++ Scripts/SQLServer/DeleteByTenantKey/DeleteByTenantKey.sql
@@ -1,14 +1,12 @@
 DECLARE @tenantKey VARCHAR(128);
 DECLARE @tenantId int;
-SET @tenantKey = '341055E5-B0D1-403C-A7B0-ADF6BB66B75C'; --replace with tenantKey from dbo.tenants
+SET @tenantKey = '123e4567-e89b-12d3-a456-426652340000'; --replace with tenantKey from dbo.tenants
 SET @tenantId = (select id from [dbo].[Tenants] where [key] = @tenantKey);
 
 delete from [dbo].[JobEvents] where TenantKey = @tenantKey;
 delete from [dbo].[Jobs] where TenantKey = @tenantKey;
 delete from [dbo].[QueueItemEvents] where TenantKey = @tenantKey;
 delete from [dbo].[QueueItems] where TenantKey = @tenantKey;
-delete from [dbo].[QUEUEMETADATA] where TenantKey = @tenantKey;
-delete from [dbo].[PROCESSMETADATA] where TenantKey = @tenantKey;
 delete from [dbo].[RobotLogs] where TenantId = @tenantId;
 delete from [read].[QueueDynamicJsonMetadata] where TenantKey = @tenantKey;
 delete from [read].[RobotDynamicJsonMetadata] where TenantKey = @tenantKey;
