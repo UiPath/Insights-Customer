f88dbb447643a4c6a7f8f423784e9f25edd589a7
Xu Zhang <xu.zhang@uipath.com>
2022-11-11 23:07:00 -0800

    fix data trimming scripts

--- Scripts/SQLServer/DataTrimmingScripts/Delete_Insights_Data_Read.sql
+++ Scripts/SQLServer/DataTrimmingScripts/Delete_Insights_Data_Read.sql
@@ -19,7 +19,49 @@ BEGIN
 	-- rebuild all Dynamic Json tables
 	UPDATE [read].[JsonSettings]
 	SET DdlOperationPerformed = 0, LastUpdatedTime = GETUTCDATE()
-	WHERE Enabled = 1
+	WHERE Enabled = 1;
+
+	-- kill all of running Migrate Data SPs
+	DECLARE @command NVARCHAR(MAX);
+
+	WHILE EXISTS (SELECT session_id 
+			  FROM sys.dm_exec_requests handle OUTER APPLY sys.fn_get_sql(handle.sql_handle) spname  
+			  WHERE spname.text LIKE '%Migrate%' AND spname.text NOT LIKE '%sys.dm_exec_requests%')
+    BEGIN
+		SELECT @command = STRING_AGG(CONCAT ('KILL ', session_id), ' ')  
+		FROM sys.dm_exec_requests handle OUTER APPLY sys.fn_get_sql(handle.sql_handle) spname  
+		WHERE spname.text LIKE '%Migrate%' AND spname.text NOT LIKE '%sys.dm_exec_requests%';
+
+    	EXEC (@command);
+	-- Aborted SP will be retried, wait 3 seconds and kill the process again. 
+		WAITFOR DELAY '00:00:03';
+    END;
+
+	-- Verify again, if there is data in the table again, truncate the table again.
+	IF EXISTS (SELECT TOP 1 * FROM [read].[RobotLogs]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[RobotLogs];
+	END;
+
+	IF EXISTS (SELECT TOP 1 * FROM [read].[Jobs]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[Jobs];
+	END;
+
+	IF EXISTS (SELECT TOP 1 * FROM [read].[JobEvents]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[JobEvents];
+	END;
+
+	IF EXISTS (SELECT TOP 1 * FROM [read].[QueueItems]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[QueueItems];
+	END;
+
+	IF EXISTS (SELECT TOP 1 * FROM [read].[QueueItemEvents]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[QueueItemEvents];
+	END;
 
     PRINT('The script executed successfully!');
 
--- Scripts/SQLServer/DataTrimmingScripts/Delete_Process_Logs_Read.sql
+++ Scripts/SQLServer/DataTrimmingScripts/Delete_Process_Logs_Read.sql
@@ -36,6 +36,28 @@ BEGIN
 	SET DdlOperationPerformed = 0, LastUpdatedTime = GETUTCDATE()
 	WHERE QueueJsonType IS NULL AND ObjectName = @ProcessName AND Enabled = 1 AND TenantKey = @TenantKey;
 
+	-- kill all of running Migrate RobotLogs SPs
+	DECLARE @command NVARCHAR(MAX);
+
+	WHILE EXISTS (SELECT session_id 
+			  FROM sys.dm_exec_requests handle OUTER APPLY sys.fn_get_sql(handle.sql_handle) spname  
+			  WHERE spname.text LIKE '%MigrateRobotLogs%' AND spname.text NOT LIKE '%sys.dm_exec_requests%')
+    BEGIN
+		SELECT @command = STRING_AGG(CONCAT ('KILL ', session_id), ' ')  
+		FROM sys.dm_exec_requests handle OUTER APPLY sys.fn_get_sql(handle.sql_handle) spname  
+		WHERE spname.text LIKE '%MigrateRobotLogs%' AND spname.text NOT LIKE '%sys.dm_exec_requests%';
+
+    	EXEC (@command);
+	-- Aborted SP will be retried, wait 3 seconds and kill the process again. 
+		WAITFOR DELAY '00:00:03';
+    END;
+
+	-- Verify again, if there is data in the table again, truncate the table again. 
+	IF EXISTS (SELECT TOP 1 * FROM [read].[RobotLogs]) 
+	BEGIN
+    	TRUNCATE TABLE [read].[RobotLogs];
+	END;
+
     PRINT('The script executed successfully!');
 
 END;
