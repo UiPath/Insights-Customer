7170fbb43a84fe3b1a31c803ef6f6a1d9aab6da1
Hongjian Niu <hongjian.niu@uipath.com>
2023-08-03 09:49:57 -0700

    Delete infor only

--- Scripts/SQLServer/DeleteByLogLevel/deleteByLogLevel.sql
+++ Scripts/SQLServer/DeleteByLogLevel/deleteByLogLevel.sql
@@ -1,5 +1,6 @@
 --LevelOrdinal: 0 unknown, 1 debug, 2 infor, 3 warn, 4 error, 5 fetal
 --if you have too many robotlogs, you can add filter for specific time range for each delete. eg: [Timestamp] < '2020-01-01 00:00:00.000'
-delete from [dbo].[RobotLogs] where LevelOrdinal in (0, 1) or (LevelOrdinal = 2 and Message not like '%execution ended');
+--if you want to delete debug log add filter "or LevelOrdinal in (0, 1)"
+delete from [dbo].[RobotLogs] where LevelOrdinal = 2 and (Message not like '%execution ended');
 --you just need to truncate read tables once
 truncate table [read].[RobotLogs];
