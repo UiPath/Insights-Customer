debf41f9f41208072809460b9f2bc618e3a1dcf6
max.lillo <max.lillo@uipath.com>
2025-02-17 16:55:06 -0500

    Accidently removed some updates from the last commit.  Added them back in.

--- Scripts/OnPrem/certificateChange.sh
+++ Scripts/OnPrem/certificateChange.sh
@@ -242,6 +242,20 @@ function remove_cert() {
     esac
 }
 
+function restart_looker() {
+    # Check the response
+    echo "Restarting looker container"
+    echo "  Executing: docker restart looker-container"
+
+    docker restart looker-container
+
+    if [ $? -ne 0 ]; then
+        echo "FATAL: Failed to restart looker container"
+        exit 1
+    fi
+
+}
+
 # Check for command flags
 while getopts ":c:s:h" opt; do
     case $opt in
@@ -283,4 +297,5 @@ check_docker_container
 set_insights_dir
 convert_pfx
 run_certificate_update
-echo "Complated Certificate Rotation Process, certificate should be updated"
+restart_looker
+echo "Completed Certificate Rotation Process, certificate should be updated"
